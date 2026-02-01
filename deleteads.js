970847a9a3fa1ce9e2ca00f4930c3c758c77c346
HidehikoKondo <udonkonet@gmal.com>
2026-02-01 22:20:02 +0900

    localstrageに保存したIDとclassの要素を削除

--- deleteads.js
+++ deleteads.js
@@ -1,23 +1,35 @@
-// 削除対象のIDとClassの文字列
-const targetStrings = [
-    "gn_delivery",
-    "mntad",
-    "google_ads_iframe",
-    "gpt_unit_",
-    "fluct-pc-sticky-ad",
-    "ad_pr_ranking",
-    "bnc_ad_",
-    "ad_jack_right",
-    "ad_jack_left",
-    "ob-smartfeed",
-    "mf-col-custom-ad",
-    "ats-glia-wrapper",
-    "ats-overlay-bottom-wrapper-rendered",
-    "pfx_interstitial",
-    "overlay_ad_pc",
-    "trv-player-container",
-    "ad_under_table_of_contents"
-];
+// 消したいIDとクラスの例
+// "gn_delivery",
+// "mntad",
+// "google_ads_iframe",
+// "gpt_unit_",
+// "fluct-pc-sticky-ad",
+// "ad_pr_ranking",
+// "bnc_ad_",
+// "ad_jack_right",
+// "ad_jack_left",
+// "ob-smartfeed",
+// "mf-col-custom-ad",
+// "ats-glia-wrapper",
+// "ats-overlay-bottom-wrapper-rendered",
+// "pfx_interstitial",
+// "overlay_ad_pc",
+// "trv-player-container",
+// "ad_under_table_of_contents"
+
+
+// 削除対象のIDとClassの文字列をchrome.storage.localから取得する
+let targetStrings = [];
+
+chrome.storage.local.get(['blockedClassesIDs'], function (result) {
+    if (result.blockedClassesIDs && Array.isArray(result.blockedClassesIDs)) {
+        targetStrings = result.blockedClassesIDs;
+        console.log("blockedClassesIDs loaded:", targetStrings);
+    } else {
+        targetStrings = [];
+        console.log("No blockedClassesIDs found in chrome.storage.local");
+    }
+});
 
 // onloadとその後５秒後に広告削除実行
 window.onload = function () {
@@ -52,6 +64,9 @@ window.addEventListener("scroll", function () {
 
 // 関数：広告を削除する
 function deleteAdd() {
+    // localStorageから最新のターゲット文字列を取得
+    const targetStrings = getTargetStrings();
+
     // 配列内の各文字列について、idまたはclassに含まれる要素をすべて取得して削除
     targetStrings.forEach(function (str) {
         var targets = Array.from(document.querySelectorAll(`[id*="${str}"], [class*="${str}"]`));
